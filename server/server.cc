0e08767d5fba779367a285afa971b668d6223d6d
Johan Anderholm <johan.anderholm@gmail.com>
2012-11-03 20:00:51 +0100

    Began implementing socket.

--- server/server.cc
+++ server/server.cc
@@ -1,20 +1,41 @@
 #include <iostream>
 #include <thread>
+#include <string>
+#include <boost/asio.hpp>
 
-using namespace std;
+#include "server.h"
+#include "batchtank.pb.h"
+
+using boost::asio::ip::tcp;
+
 
 void
 hello()
 {
-    cout << "Hello from thread " << endl;
+    std::cout << "Hello from thread " << std::endl;
 }
+
 
 int
 main()
 {
-    thread t1(hello);
+    try {
+        boost::asio::io_service io_service;
+        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 54000));
 
-    t1.join();
+        while(true) {
+            tcp::socket socket(io_service);
+            acceptor.accept(socket);
+
+            std::string message("Hello World!");
+
+            boost::system::error_code ignored_error;
+            boost::asio::write(socket, boost::asio::buffer(message),
+                               ignored_error);
+        }
+    } catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }

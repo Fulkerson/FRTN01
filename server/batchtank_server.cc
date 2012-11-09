654f3a25404a3d13b7e68e5d3c25c124ea29a0d5
Johan Anderholm <johan.anderholm@gmail.com>
2012-11-09 18:56:40 +0100

    Finished utils for parsing delimited.

--- server/batchtank_server.cc
+++ server/batchtank_server.cc
@@ -1,18 +1,101 @@
 #include <iostream>
 #include <string>
+#include <time.h>
+#include <cerrno>
 #include <boost/asio.hpp>
 #include <boost/thread.hpp>
 
 #include "batchtank_server.h"
+#include "utils.h"
+
+/* Used to silence compiler when unused vars. */
+#define UNUSED(expr) if(0) { (void)(expr); }
 
 using boost::asio::ip::tcp;
 using namespace batchtank;
 
+/* Convenience function for adding two timespecs */
+static timespec
+add_timespec(timespec t1, timespec t2)
+{
+    timespec t;
+    t.tv_sec  = t1.tv_sec + t2.tv_sec;
+    t.tv_sec += (t1.tv_nsec + t2.tv_nsec) / 999999999;
+    t.tv_nsec = (t1.tv_nsec + t2.tv_nsec) % 999999999;
+    return t;
+}
 
-ConnectionThread::ConnectionThread(std::shared_ptr<tcp::iostream> stream) :
-    m_Stream(stream) {}
+PeriodicTask::PeriodicTask(int32_t period, std::function<void()> task) :
+    period(period), task(task){}
 
-   
+PeriodicTask::~PeriodicTask()
+{
+    this->stop();
+}
+
+void
+PeriodicTask::start()
+{
+    run = true;
+    m_Thread = boost::thread(&PeriodicTask::execute, this);
+}
+
+void
+PeriodicTask::stop()
+{
+    run = false;
+    m_Thread.join();
+}
+
+void
+PeriodicTask::execute()
+{
+    timespec t; 
+    timespec p;
+
+    /* Period is in millis, convert to sec and nanos */
+    p.tv_sec  = period / 1000;
+    p.tv_nsec = (period % 1000) * 1000000L;
+
+    clock_gettime(CLOCK_MONOTONIC, &t);
+
+    while (run) {
+        /* Execute given task */
+        task();
+
+        /* Calc and sleep until next period. */
+        t = add_timespec(t, p);
+
+        while (true) {
+            if (clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, nullptr)) {
+                switch (errno) {
+                    case EINTR:
+                        std::cerr << "EINTR" << std::endl;
+                        /* Try again if interupted */
+                        continue;
+                    case EFAULT:
+                        std::cerr << "EFAULT" << std::endl;
+                        break;
+                    case EINVAL:
+                        std::cerr << "EINVAL" << std::endl;
+                        break;
+                }
+                run = false;
+                std::cerr << "ERROR: Leaving periodic task" << std::endl;
+                break;
+            } else {
+                /* All good */
+                break;
+            }
+        }
+    }
+}
+
+
+ConnectionThread::ConnectionThread(std::shared_ptr<tcp::socket> m_Socket) :
+    m_Socket(m_Socket) {}
+
+
 void
 ConnectionThread::start()
 {
@@ -28,11 +111,27 @@ ConnectionThread::run()
         boost::posix_time::seconds periodTime(3);
         boost::system::error_code ignored_error;
 
-        while(true) {
-            std::cout << "hello world!" << std::endl;
-            *m_Stream << "hello world!" << std::endl;
-            boost::this_thread::sleep(periodTime);
+        boost::asio::streambuf obuf;
+        boost::asio::streambuf ibuf;
+        std::ostream os(&obuf);
+       
+        PeriodicTask periodic(100, [this, &obuf, &os]() {
+                google::protobuf::io::OstreamOutputStream raw_os(&os);
+                google::protobuf::io::CodedOutputStream cos(&raw_os);
+                cos.WriteVarint64(100);
+                boost::asio::write(*m_Socket, obuf);
+            });
+        periodic.start();
+
+        messages::BaseMessage msg;
+        MessageParser condition(msg, ibuf);
+
+        while (true) {
+            int nbr_read = boost::asio::read(*m_Socket, ibuf, condition);
+            std::cout << "Number of bytes read: " << nbr_read << std::endl;
+            std::cout << "DEBUG: " << msg.DebugString() << std::endl;
         }
+
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
@@ -49,11 +148,10 @@ main()
         tcp::acceptor acceptor(io_service, endpoint);
 
         while(true) {
-            std::shared_ptr<tcp::iostream> stream(new tcp::iostream());
-            acceptor.accept(*(stream->rdbuf()));
+            std::shared_ptr<tcp::socket> socket(new tcp::socket(io_service));
+            acceptor.accept(*socket);
 
-            (new ConnectionThread(stream))->start();
-
+            (new ConnectionThread(socket))->start();
         }
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
--- server/batchtank_server.h
+++ server/batchtank_server.h
@@ -1,6 +1,7 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <google/protobuf/io/zero_copy_stream_impl.h>
 #include "batchtank.pb.h"
 
 namespace batchtank {
@@ -10,14 +11,40 @@ namespace messages = batchtank_messages;
 
 class ConnectionThread {
     public:
-        ConnectionThread(std::shared_ptr<boost::asio::ip::tcp::iostream>);
+        ConnectionThread(std::shared_ptr<boost::asio::ip::tcp::socket>);
         void run();
         void start();
     private:
         boost::thread m_Thread;
-        std::shared_ptr<boost::asio::ip::tcp::iostream> m_Stream;
+        std::shared_ptr<boost::asio::ip::tcp::socket> m_Socket;
 };
 
+class PeriodicTask {
+    public:
+        PeriodicTask(int32_t period, std::function<void()> task);
+        ~PeriodicTask();
+        void start();
+        void stop();
+    private:
+        boost::thread m_Thread;
+        int32_t period;
+        /* Task to be executed on a periodic basis */
+        std::function<void()> task;
+        void execute();
+        bool run;
+};
+
+/* Monitor class used to interface with the batch process. */
+class IORegistry {
+    public:
+        /* Used for locking when writing/reading to the batch process. */
+        boost::mutex mutex;
+
+
+    private:
+        /* Signal copies, allow plotter to read control signals etc. */
+       int32_t inletpump;
+};
 }
 
 #endif

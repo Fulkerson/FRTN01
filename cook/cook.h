76264a2b6dd11db36a5c1eda053c15acdea4f723
Mikael Nilsson <svengunnar.mikael@gmail.com>
2012-11-20 16:15:44 +0100

    One can now set the rates of the cooker, though getting values of port does not work yet ZzZ..

--- cook/cook.h
+++ cook/cook.h
@@ -1,19 +1,26 @@
 
 enum read_target {
-	NILL, TEMP, LEVEL, IN_PUMP_RATE, OUT_PUMP_RATE, HEATER_RATE, 
-	MIXER_RATE, COOLER_RATE
+	TEMP , 
+	LEVEL, 
+	IN_PUMP_RATE, 
+	OUT_PUMP_RATE, 
+	HEATER_RATE, 
+	MIXER_RATE, 
+	COOLER_RATE
 };
 
 enum set_target {
-	HEATER, COOLER, IN_PUMP, OUT_PUMP, MIXER
+	HEATER, 
+	COOLER, 
+	IN_PUMP, 
+	OUT_PUMP, 	
+	MIXER
 };
 
-/* Methods returns false if they fail */
 
-int get(enum channel);				/* Get current value of target channel */
+int get(enum read_target);			/* Get current value of target channel */
 
-/* char value may not use the most significant bit */	
-int set(enum set_target, char value);		/* Set a value to target channel */
+int set(enum set_target, int value);		/* Set a value to target channel */
 
 int init(const char*);				/* Setup communication */
 

0468ed23959dd06a3d916246364ce0c69df94248
Johan Anderholm <johan.anderholm@gmail.com>
2012-11-22 10:52:56 +0100

    Cleaned up.

--- plot_client/http_server.py
+++ plot_client/http_server.py
@@ -1,9 +1,12 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from random import SystemRandom
+from urlparse import urlparse, parse_qs
+
 import json
 import re
+import os
 
-DIR = '../plot/'
+DIR = 'batchtank/static'
 
 class RequestHandler(BaseHTTPRequestHandler):
     _rand = SystemRandom()
@@ -18,28 +21,22 @@ class RequestHandler(BaseHTTPRequestHandler):
 
                 self.wfile.write(f.read())
                 f.close()
-            elif (self.path.startswith('/data.php')):
+            elif (self.path.startswith('/data')):
                 self.send_response(200)
                 self.send_header('Cache-Control', 'no-cache, must-revalidate')
                 self.send_header('Content-type', 'application/json;charset=utf-8')
                 self.end_headers()
-
-                res = re.findall('([a-zA-Z]+)=([0-9\.\,]+)', self.path)
-                print res
-                out = '{'
-                vals = []
-                for curve in res:
-                    vals.append('"' + curve[0] + '":[[' + str(int(curve[1]) + 1) + ',' + str(int(self._rand.random() * 30 + 20)) + ']]')
-                if (len(vals) == 0):
-                    vals.append('"ref":[[12,37]]')
-                    vals.append('"out":[[5,12]]')
-                    vals.append('"in":[[2,52]]')
-                out += ','.join(vals) + '}'
-                print out
+                query = parse_qs(urlparse(self.path)[4])
+                print query
+                out = json.dumps({
+                    'ref': [[12, 37]],
+                    'out': [[5, 12]],
+                    'in': [[2, 25]]
+                    })
                 self.wfile.write(out)
             elif (self.path == '/' or self.path == '/index.html' or self.path == '/index.htm'):
                 # Serve index!
-                f = open(DIR + 'index.htm')
+                f = open(DIR + '/index.htm')
 
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -50,11 +47,12 @@ class RequestHandler(BaseHTTPRequestHandler):
             else:
                 self.send_error(404, 'Aeh naee... Di blidde 404: ' + self.path)
         except IOError:
+            raise
             self.send_error(404, 'Aeh naee... Di blidde 404.')
 
 def main():
     try:
-        server = HTTPServer(('', 80), RequestHandler)
+        server = HTTPServer(('', 8080), RequestHandler)
         print 'Server running...'
         server.serve_forever()
     except KeyboardInterrupt:

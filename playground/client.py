087644ddac6b549ba53a336f7cbb2fb1b9dd4500
Johan Anderholm <johan.anderholm@gmail.com>
2012-12-04 15:38:58 +0100

    Client script moved to playground.

--- playground/client.py
+++ playground/client.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import signal
 import subprocess
 import pyinotify
 
-PID_BINARY = "./pid_client"
-PID_CONFIG = "pid_client.ini"
-
+PID_BINARY = "../pid_client/pid_client"
 
 class EventHandler(pyinotify.ProcessEvent):
-    def __init__(self, proc):
+    def __init__(self, proc, name):
         self.proc = proc
+        self.name = name
 
     def process_IN_CLOSE_WRITE(self, event):
-        if event.name == PID_CONFIG:
+        if event.name == self.name:
             self.proc.send_signal(signal.SIGUSR1)
 
 
-def main():
+def main(config):
     # Start process
-    client = subprocess.Popen([PID_BINARY, PID_CONFIG])
+    client = subprocess.Popen([PID_BINARY, config])
 
     # Set up handling
     wm = pyinotify.WatchManager()
-    handler = EventHandler(client)
+    handler = EventHandler(client, config)
     notifier = pyinotify.Notifier(wm, handler)
 
-    config_dir = os.path.dirname(PID_CONFIG)
+    config_dir = os.path.dirname(config)
 
     # Watch config
     wm.add_watch(config_dir, pyinotify.ALL_EVENTS)
@@ -40,5 +40,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print "USAGE: client [CONFIG]"
+    else:
+        main(sys.argv[1])
 
